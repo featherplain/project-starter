22428028d536808156d4afae9e18345a20cae938
featherplain <info@featherplain.com>
2016-01-04 16:12:40 +0900

    switch foundation version from 5 to 6

--- src/js/app/script.js
+++ src/js/app/script.js
@@ -1,22 +1,6 @@
-// magnific popup
-$(function () {
-  $('.modal__btn').magnificPopup({
-    type: 'inline',
-    preloader: false,
-    focus: '#username',
-    modal: true,
+(function($) {
+  $(document).ready(function(){
+    // something here
   });
-  $(document).on('click', '.modal__btn--dismiss', function (e) {
-    e.preventDefault();
-    $.magnificPopup.close();
-  });
-});
-// add action highlight.js
-hljs.initHighlightingOnLoad();
-// initialization of highlight.js
-$(document).ready(function() {
-  $('pre').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-});
-    
+})(jQuery);
+
