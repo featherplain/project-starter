f094d4ae783a67e90ee55197b526264da25d3078
featherplain <info@featherplain.com>
2016-09-27 21:18:19 +0900

    init smoothscroll

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -14,7 +14,7 @@ gulp.task('watch', () => {
   gulp.watch([paths.imageDest + 'sprite/*.png'], ['sprite']);
   gulp.watch([paths.imagePath + 'sprite-svg/*.svg'], ['sprite:svg'])
   gulp.watch([paths.jadePath  + '**/*.jade'], ['jade']);
-  gulp.watch([paths.jsPath    + '*.js'], ['bundle:js']);
+  gulp.watch([paths.jsPath    + '**/*.js'], ['bundle:js']);
   gulp.watch([paths.scssPath  + '**/*.scss'], ['sass']);
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-import './app/scroll';
+// import './classes/scroll';
+
+import smoothScroll from 'smoothscroll';
+
+const exampleBtn = document.getElementById('start');
+const exampleDestination = document.getElementById('end');
+
+const handleClick = event => {
+  event.preventDefault();
+
+  smoothScroll(exampleDestination);
+};
+exampleBtn.addEventListener('click', handleClick);
